4ffb488072cefa16267ee1b4a397014db07490fe
Gorileng <keneilwe963@gmail.com>
2023-05-17 11:06:50 -0700

    simple shell

--- builtin.c
+++ builtin.c
@@ -1,23 +1,23 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: exits with a given exit status
- * (0) if info.argv[0] != "exit"
+ * _myexit - exit shell
+ * @info: Structure contains the potential arguments. that are used to maintain
+ * constant function of the prototype.
+ * Return: exit with given exit status
+ * (0) info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1]) /* If there is an exit arguement */
+	if (info->argv[1]) /* If there's exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
 		{
 			info->status = 2;
-			print_error(info, "Illegal number: ");
+			print_error(info, "the Illegal number: ");
 			_eputs(info->argv[1]);
 			_eputchar('\n');
 			return (1);
@@ -30,18 +30,18 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
+ * _mycd - change current directory of process
+ * @info: Structure contains the potential arguments. that are used to maintain
+ * constant function of the prototype.
  * Return: Always 0
  */
 int _mycd(info_t *info)
 {
-	char *s, *dir, buffer[1024];
+	char *w, *dir, buffer[1024];
 	int chdir_ret;
 
-	s = getcwd(buffer, 1024);
-	if (!s)
+	w = getcwd(buffer, 1024);
+	if (!w)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
@@ -56,7 +56,7 @@ int _mycd(info_t *info)
 	{
 		if (!_getenv(info, "OLDPWD="))
 		{
-			_puts(s);
+			_puts(w);
 			_putchar('\n');
 			return (1);
 		}
@@ -68,7 +68,7 @@ int _mycd(info_t *info)
 		chdir_ret = chdir(info->argv[1]);
 	if (chdir_ret == -1)
 	{
-		print_error(info, "can't cd to ");
+		print_error(info, "cannot cd to ");
 		_eputs(info->argv[1]), _eputchar('\n');
 	}
 	else
@@ -80,9 +80,9 @@ int _mycd(info_t *info)
 }
 
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
+ * _myhelp - change current directory of process
+ * @info: Structure contains the potential arguments. that are used to maintain
+ * constant function of the prototype.
  * Return: Always 0
  */
 int _myhelp(info_t *info)
--- builtin1.c
+++ builtin1.c
@@ -1,12 +1,15 @@
 #include "shell.h"
 
+
 /**
- * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * _myhistory - display history list, one command by line, prior
+ *              with the line numbers, starting from 0.
+ * @info: Structure contains the potential arguments. that are used to maintain
+ *        constant function of the prototype.
  *  Return: Always 0
  */
+
+
 int _myhistory(info_t *info)
 {
 	print_list(info->history);
@@ -14,43 +17,43 @@ int _myhistory(info_t *info)
 }
 
 /**
- * unset_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
+ * unset_alias - set alias to the string
+ * @info: the parameter struct
+ * @str: string alias
  *
- * Return: Always 0 on success, 1 on error
+ * Return: Always 0 success, 1 error
  */
 int unset_alias(info_t *info, char *str)
 {
-	char *p, c;
+	char *q, b;
 	int ret;
 
-	p = _strchr(str, '=');
-	if (!p)
+	q = _strchr(str, '=');
+	if (!q)
 		return (1);
-	c = *p;
-	*p = 0;
+	b = *q;
+	*q = 0;
 	ret = delete_node_at_index(&(info->alias),
 		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*p = c;
+	*q = b;
 	return (ret);
 }
 
 /**
- * set_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
+ * set_alias - set alias to the string
+ * @info: the parameter struct
+ * @str: string alias
  *
- * Return: Always 0 on success, 1 on error
+ * Return: Always 0 success, 1 error
  */
 int set_alias(info_t *info, char *str)
 {
-	char *p;
+	char *q;
 
-	p = _strchr(str, '=');
-	if (!p)
+	q = _strchr(str, '=');
+	if (!q)
 		return (1);
-	if (!*++p)
+	if (!*++q)
 		return (unset_alias(info, str));
 
 	unset_alias(info, str);
@@ -58,22 +61,22 @@ int set_alias(info_t *info, char *str)
 }
 
 /**
- * print_alias - prints an alias string
- * @node: the alias node
+ * print_alias - print alias string
+ * @node: alias node
  *
- * Return: Always 0 on success, 1 on error
+ * Return: Always 0 success, 1 error
  */
 int print_alias(list_t *node)
 {
-	char *p = NULL, *a = NULL;
+	char *q = NULL, *a = NULL;
 
 	if (node)
 	{
-		p = _strchr(node->str, '=');
-		for (a = node->str; a <= p; a++)
+		q = _strchr(node->str, '=');
+		for (a = node->str; a <= q; a++)
 		_putchar(*a);
 		_putchar('\'');
-		_puts(p + 1);
+		_puts(q + 1);
 		_puts("'\n");
 		return (0);
 	}
@@ -81,15 +84,15 @@ int print_alias(list_t *node)
 }
 
 /**
- * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * _myalias - imitates alias builtin (man alias)
+ * @info: Structure contains the potential arguments. that are used to maintain
+ *          constant function of the prototype.
  *  Return: Always 0
  */
 int _myalias(info_t *info)
 {
 	int i = 0;
-	char *p = NULL;
+	char *q = NULL;
 	list_t *node = NULL;
 
 	if (info->argc == 1)
@@ -104,8 +107,8 @@ int _myalias(info_t *info)
 	}
 	for (i = 1; info->argv[i]; i++)
 	{
-		p = _strchr(info->argv[i], '=');
-		if (p)
+		q = _strchr(info->argv[i], '=');
+		if (q)
 			set_alias(info, info->argv[i]);
 		else
 			print_alias(node_starts_with(info->alias, info->argv[i], '='));
